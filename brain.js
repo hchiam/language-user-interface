ab49d6617a2445e7c271e5147e01454f681b12ab
hchiam <hchiam@github.com>
2017-07-20 20:31:32 -0400

    var -> let (like private variables in java, within scope)

--- brain.js
+++ brain.js
@@ -8,8 +8,8 @@ window.addEventListener('offline', function(e) { say("You've lost your internet 
 window.addEventListener('online', function(e) { say("We're back online now."); });
 
 function welcome() {
-  var today = new Date()
-  var currentHour = today.getHours()
+  let today = new Date()
+  let currentHour = today.getHours()
   if (currentHour < 12) {
     // 00:00 - 11:59
     say('Good morning. How may I help you?');
@@ -22,7 +22,7 @@ function welcome() {
   }
 }
 
-var unfamiliarUser = setTimeout(function(){
+let unfamiliarUser = setTimeout(function(){
   introSelf();
 }, 7000);
 
@@ -35,7 +35,7 @@ function introSelf() {
        you can enter text verbally into the textbox.");
 }
 
-var delayedAction;
+let delayedAction;
 
 function converse() {
   clearTimeout(unfamiliarUser); // user is using the interface
@@ -57,9 +57,9 @@ function userWantsAnswerNow(event, kd) {
 
 function countDownWaiting() {
   document.getElementById('countDownWaiting').value += 20;
-  var percent = document.getElementById('countDownWaiting').value;
+  let percent = document.getElementById('countDownWaiting').value;
   if (percent >= 100) {
-    var heard = listen();
+    let heard = listen();
     if (heard) {
       updateMessageLog(heard, 'user');
       reply(heard);
@@ -76,10 +76,10 @@ function updateMessageLog(message, who) {
   // ignore the interrupt say(' ')
   if (message != ' ') {
     // set up style based on LUI/user speaking
-    var id = ' id="log-' + who + '"';
+    let id = ' id="log-' + who + '"';
     // create message
-    var nextMessage = '<p' + id + '>' + message + '</p>';
-    var logSoFar = document.getElementById('messageLog').innerHTML;
+    let nextMessage = '<p' + id + '>' + message + '</p>';
+    let logSoFar = document.getElementById('messageLog').innerHTML;
     // show message
     document.getElementById('messageLog').innerHTML = nextMessage + logSoFar;
   }
@@ -98,7 +98,7 @@ function say(sentence) {
 }
 
 function listen() {
-  var heard = document.getElementById("input").value;
+  let heard = document.getElementById("input").value;
   // remove trailing/leading spaces, set to lowercase, and remove punctuation
   heard = heard.trim().toLowerCase();
   heard = heard.replace(/[.,\/#!?$%\^&\*;:{}<>+=\-_`"~()]/g,'');
@@ -118,14 +118,14 @@ function reply(heard) {
   // TODO: add more functionality
   // TODO: make more modular
 
-  var heardRecognized = false;
+  let heardRecognized = false;
 
   heardRecognized |= heardInterrupt(heard);
   heardRecognized |= heardPleasantries(heard);
   heardRecognized |= heardSearch(heard);
 
   if (!heardRecognized) {
-    var sentence = "You said: " + heard + '...' + "Sorry, I currently don't have a pre-programmed response to that.";
+    let sentence = "You said: " + heard + '...' + "Sorry, I currently don't have a pre-programmed response to that.";
     // need '...' to make an audible pause
     say(sentence);
   }
@@ -134,15 +134,15 @@ function reply(heard) {
 function didHear(heard, listOfChecks=[], checkType='exact match') {
   // using separate for-loops instead of checking checkType every time
   if (checkType === 'exact match') {
-    for (var i in listOfChecks) {
+    for (let i in listOfChecks) {
       if (heard === listOfChecks[i]) return true;
     }
   } else if (checkType === 'starts with') {
-    for (var i in listOfChecks) {
+    for (let i in listOfChecks) {
       if (heard.startsWith(listOfChecks[i])) { return true; }
     }
   } else if (checkType === 'ends with') {
-    for (var i in listOfChecks) {
+    for (let i in listOfChecks) {
       if (heard.endsWith(listOfChecks[i])) { return true; }
     }
   }
@@ -214,7 +214,7 @@ function heardSearch(heard) {
                         "who's ", 'who is ', 'who are ', 'who was ', 'who were ',
                         'search for ', 'tell me about '];
   if (didHear(heard, signalPhrases, 'starts with')) {
-    var words = removeSignalPhrases(heard,signalPhrases);
+    let words = removeSignalPhrases(heard,signalPhrases);
     searchDefinition(words);
     return true;
   }
@@ -262,16 +262,16 @@ function askingTime(heard) {
                      'what time is it now', "what time's it",
                      'what is the time', 'what is the time right now',
                      "what's the time", "what's the time right now"])) {
-    var d = new Date();
-    var t = d.toLocaleTimeString();
+    let d = new Date();
+    let t = d.toLocaleTimeString();
     say('It is ' + t);
     return true;
   }
   // check date
   if (didHear(heard,["what's today's date", "what's the date today",
                      'what is the date today','what day is it today'])) {
-    var d = new Date();
-    var t = d.toDateString();
+    let d = new Date();
+    let t = d.toDateString();
     say('It is ' + t);
     return true;
   }
@@ -281,7 +281,7 @@ function askingTime(heard) {
 
 function getLocation(func) { // e.g.: getLocation passes myLocation to getWeather(myLocation)
 
-  var locationFull;
+  let locationFull;
 
   if (navigator.geolocation === undefined) {
     alert("Sorry, I'm unable to use geolocation in this browser. Try another browser.")
@@ -290,11 +290,11 @@ function getLocation(func) { // e.g.: getLocation passes myLocation to getWeathe
 
     // get location latitude and longitude
     navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
 
         // get location address
-        var urlAPICall = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + long;
+        let urlAPICall = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + long;
         $.getJSON(urlAPICall, function(response){
 
             if (response.status != 'OK') {
@@ -302,17 +302,17 @@ function getLocation(func) { // e.g.: getLocation passes myLocation to getWeathe
             } else {
 
                 // set up for getting the parts we want
-                var resultObject = response.results[0].address_components;
-                var locationFull = '';
-                var city, adminAreaLvl1, country;
+                let resultObject = response.results[0].address_components;
+                let locationFull = '';
+                let city, adminAreaLvl1, country;
 
                 // get the parts we want
-                for (var i in resultObject) {
+                for (let i in resultObject) {
                   // put together full location description
                   locationFull += resultObject[i].long_name + ',';
                   // check location component types
-                  var types = resultObject[i].types;
-                  for (var j in types) {
+                  let types = resultObject[i].types;
+                  for (let j in types) {
                     // get specific location components
                     if (types[j] === 'locality') {
                       city = resultObject[i].long_name;
@@ -347,8 +347,8 @@ function getLocation(func) { // e.g.: getLocation passes myLocation to getWeathe
 
 function askingWeather(heard) {
   // general weather description
-  var regexHow = new RegExp("^how('s| is)? (the )?(weather|forecast)( like)?( today)?( like)?");
-  var regexWhat = new RegExp("^what('s| is)? (the )?(weather|forecast)( like( today)?| today( like)?)");
+  let regexHow = new RegExp("^how('s| is)? (the )?(weather|forecast)( like)?( today)?( like)?");
+  let regexWhat = new RegExp("^what('s| is)? (the )?(weather|forecast)( like( today)?| today( like)?)");
   // TODO: add "^(.+) at (.+) (o'clock)?$"
   matches = regexHow.test(heard) || regexWhat.test(heard);
   if (matches) {
@@ -358,7 +358,7 @@ function askingWeather(heard) {
   }
 
   // temperature
-  var regexTemp = new RegExp("^(how|what)('s| is) (the )?temperature( like)?( today)?( like)?");
+  let regexTemp = new RegExp("^(how|what)('s| is) (the )?temperature( like)?( today)?( like)?");
   // TODO: add "^(.+) at (.+) (o'clock)?$"
   matches = regexTemp.test(heard);
   if (matches) {
@@ -372,12 +372,12 @@ function askingWeather(heard) {
 
 function getWeather(myLocation) {
   // get weather statement for that location (text)
-  var urlAPICall = "https://query.yahooapis.com/v1/public/yql?q=select item.condition.text from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + myLocation + "')&format=json";
+  let urlAPICall = "https://query.yahooapis.com/v1/public/yql?q=select item.condition.text from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + myLocation + "')&format=json";
   $.getJSON(urlAPICall, function(data) {
     // text
-    var weatherInfo = data.query.results.channel.item.condition.text;
+    let weatherInfo = data.query.results.channel.item.condition.text;
     say("It's " + weatherInfo.toLowerCase() + ' around ' + myLocation + '.');
-    // var wind = data.query.results.channel.wind;
+    // let wind = data.query.results.channel.wind;
     // alert(data.query);
     // say(wind.chill);
   });
@@ -385,11 +385,11 @@ function getWeather(myLocation) {
 
 function getTemperature(myLocation) {
   // get temperature for that location (temp)
-  var urlAPICall = "https://query.yahooapis.com/v1/public/yql?q=select item.condition.temp from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + myLocation + "') and u='c'&format=json";
+  let urlAPICall = "https://query.yahooapis.com/v1/public/yql?q=select item.condition.temp from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + myLocation + "') and u='c'&format=json";
   $.getJSON(urlAPICall, function(data) {
     // temp
-    var temp = data.query.results.channel.item.condition.temp;
-    var tempRefPt = getTemperatureRefPt(temp);
+    let temp = data.query.results.channel.item.condition.temp;
+    let tempRefPt = getTemperatureRefPt(temp);
     say("It's " + temp + ' degrees Celsius around ' + myLocation + '. ' + tempRefPt);
   });
 }
@@ -419,15 +419,15 @@ function askingDirections(heard) {
                   /^get directions (to|for) (a |the nearest |the closest )?(.+)/,
                   /^how (can|do) (i|we) (get|go) to (a |the nearest |the closest )?(.+)/
                 ];
-  var matchFound = [];
-  var searchFor;
-  for (var i in regex) {
+  let matchFound = [];
+  let searchFor;
+  for (let i in regex) {
     matchFound[i] = heard.match(regex[i])
     if (matchFound[i]) {
       searchFor = matchFound[i][matchFound[i].length-1];
       // go to map
       // https://www.google.com/maps/dir/here/{searchFor}
-      var urlAPICall = 'https://www.google.com/maps/dir/here/';
+      let urlAPICall = 'https://www.google.com/maps/dir/here/';
       urlAPICall += searchFor.replace(' ','+');
       say("I'm now opening a Google maps results page for the closest " + searchFor + '.');
       tryOpeningWindow(urlAPICall);
@@ -451,10 +451,10 @@ function askingReminder(heard) {
     heard = swapWords(heard, pronounSwaps);
 
     // get what and when to remind
-    var regex = new RegExp("^(.+) in (.+) (minutes?|hours?|seconds?)$");
+    let regex = new RegExp("^(.+) in (.+) (minutes?|hours?|seconds?)$");
     // TODO: add "^(.+) at (.+) (o'clock)?$"
     matches = regex.test(heard);
-    var remindWhat, remindWhen, timeUnits;
+    let remindWhat, remindWhen, timeUnits;
     if (matches) {
       remindWhat = heard.match(regex)[1].replace(/^to /g,'');
       remindWhen = heard.match(regex)[2];
@@ -492,22 +492,22 @@ function reminderTimer(remindWhat, remindWhen, timeUnits) {
       remindWhen = 60000; // default to one minute
       break;
   }
-  var unfamiliarUser = setTimeout(function(){
+  let unfamiliarUser = setTimeout(function(){
     say(remindWhat)
   }, remindWhen);
 }
 
 function swapWords(sentence, dictionary) {
-  var words = sentence.split(' ');
-  for (var i in words) {
-    var word = words[i];
+  let words = sentence.split(' ');
+  for (let i in words) {
+    let word = words[i];
     if (word in dictionary) words[i] = dictionary[word];
   }
   return words.join(' ');
 }
 
 function askingMath(heard) {
-  var possibleExpression = heard.replace('what is ').replace("what's ");
+  let possibleExpression = heard.replace('what is ').replace("what's ");
   possibleExpression = possibleExpression.replace(/[.,\/#!?$%\^&\*;:{}<>+=\-_`"~()]/g,''); // make safer
   const mathWords = {'one':'1','two':'2','three':'3','four':'4','five':'5',
                     'six':'6','seven':'7','eight':'8','nine':'9','zero':'0',
@@ -520,8 +520,8 @@ function askingMath(heard) {
                     'and':'',
                     'point':'.','decimal':'.',
                     'plus':'+', 'minus':'-', 'divided by':'/', 'times':'*', 'multiplied by':'*', 'to the power of':'^'};
-  for (var key in mathWords) {
-    var toReplaceAllInstances = new RegExp(key, "g");
+  for (let key in mathWords) {
+    let toReplaceAllInstances = new RegExp(key, "g");
     possibleExpression = possibleExpression.replace(toReplaceAllInstances, mathWords[key]);
   }
   // TODO: split by spaces and check if pairs of current-adjacent words are integers or one of them equals 'and', then combine
@@ -538,8 +538,8 @@ function askingMath(heard) {
 
 function safeForMath(expression) {
   const mathy = '1234567890+-*/^';
-  for (var i in expression) {
-    var letter = expression[i];
+  for (let i in expression) {
+    let letter = expression[i];
     if (!mathy.includes(letter)) {
       return false;
     }
@@ -551,7 +551,7 @@ function askingAnalogy(heard) {
   const regex1 = new RegExp("^what('s| is) (an? |the )?(.+)( like)+?");
   const regex2 = new RegExp("^what('s| is) (an? )?analogy for (an? )?(.+)");
   const regex3 = new RegExp("^find (an? )?analogy for (an? )?(.+)");
-  var matches, words;
+  let matches, words;
   matches = heard.match(regex1);
   if (matches) {
     searchAnalogy(matches[3]);
@@ -578,7 +578,7 @@ function searchAnalogy(words) {
 
 function removeSignalPhrases(heard, signalPhrases) {
   // find first match for start of sentence, remove, and stop checking
-  for (var i in signalPhrases) {
+  for (let i in signalPhrases) {
     if (heard.startsWith(signalPhrases[i])) {
       heard = heard.replace(signalPhrases[i],'');
       break;
@@ -591,7 +591,7 @@ function removeSignalPhrases(heard, signalPhrases) {
 
 function searchLocation(heard) {
 
-  var searchWords, regex, matches, searchFor, searchIn;
+  let searchWords, regex, matches, searchFor, searchIn;
 
   // note: TRICKY!: check more restrictive first! (otherwise capture too much)
 
@@ -621,7 +621,7 @@ function searchLocation(heard) {
   // either way:
   if (matches) {
     // https://www.google.com/maps/search/?api=1&query={searchWords}
-    var urlAPICall = 'https://www.google.com/maps/search/?api=1&query=';
+    let urlAPICall = 'https://www.google.com/maps/search/?api=1&query=';
     urlAPICall += searchWords;
     say("I'm now opening a Google maps results page for: " + searchFor + '.');
     tryOpeningWindow(urlAPICall);
@@ -634,16 +634,16 @@ function searchLocation(heard) {
 function searchDefinition(words) {
   // search wikipedia
 
-  // var urlAPICall = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&exsentences=1&callback=?&titles=';
-  var urlAPICall = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=1&exintro&explaintext=&indexpageids=true&format=json&generator=search&gsrlimit=1&callback=?&gsrsearch=';
+  // let urlAPICall = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&exsentences=1&callback=?&titles=';
+  let urlAPICall = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=1&exintro&explaintext=&indexpageids=true&format=json&generator=search&gsrlimit=1&callback=?&gsrsearch=';
   urlAPICall += words;
   // tryOpeningWindow(urlAPICall);
   $.getJSON(urlAPICall, function(data) {
     try {
       // wikipedia returns pages with page id's, so try to get the extract of the first one
-      var pageInfo = Object.values(data.query.pages)[0];
-      var summary = pageInfo.extract;
-      var title = pageInfo.title;
+      let pageInfo = Object.values(data.query.pages)[0];
+      let summary = pageInfo.extract;
+      let title = pageInfo.title;
       if (isSubstring(summary,'may refer to')) {
         say('"' + capitalizeFirstLetter(words) + '"' + " can mean a few different things. I'm opening the Wikipedia disambiguation page.")
         tryOpeningWindow('https://www.wikipedia.org/wiki/' + words);
@@ -666,13 +666,13 @@ function isSubstring(string, substring) {
 function searchQuestion(heard) {
   // search duckduckgo
 
-  // var urlAPICall = 'https://api.duckduckgo.com/?format=json&pretty=1&q=';
-  var urlAPICall = 'https://api.duckduckgo.com/?q=';
+  // let urlAPICall = 'https://api.duckduckgo.com/?format=json&pretty=1&q=';
+  let urlAPICall = 'https://api.duckduckgo.com/?q=';
   urlAPICall += heard;
   say("I'm now opening a search results page.");
   tryOpeningWindow(urlAPICall);
 
-  // var urlAPICall = 'http://api.duckduckgo.com/?format=jsonp&pretty=1&q=';
+  // let urlAPICall = 'http://api.duckduckgo.com/?format=jsonp&pretty=1&q=';
   // urlAPICall += words;
   // // tryOpeningWindow(urlAPICall);
   // alert('right above getJSON')
@@ -680,16 +680,16 @@ function searchQuestion(heard) {
   //   alert('got in')
   //   alert(data);
   //   // wikipedia returns pages with page id's, so try to get the extract of the first one
-  //   // var page = Object.values(data.RelatedTopics)[0];
-  //   // var summary = page.text;
-  //   // var title = page.FirstURL.replace('https://duckduckgo.com/','').replace('_',' ');
+  //   // let page = Object.values(data.RelatedTopics)[0];
+  //   // let summary = page.text;
+  //   // let title = page.FirstURL.replace('https://duckduckgo.com/','').replace('_',' ');
   //   // if (title.toLowerCase() != words) say("I'm not sure this is what you're looking for, but here's what I found.")
   //   // say("duck duck go says: " + summary); // alert(Object.values(data.query.pages)[0].extract)
   // });
 }
 
 function tryOpeningWindow(url) { // notice and tell user to unblock if can't open window
-  var newWindow = window.open(url);
+  let newWindow = window.open(url);
   if(!newWindow || newWindow.closed || typeof newWindow.closed === 'undefined') {
     say('Sorry... Please authorize me to open new windows for you... You may be using an ad blocker.');
   } else {
